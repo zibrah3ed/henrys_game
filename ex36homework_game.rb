5ac105f0df018b8c4f96fd702f0fed9e5a9dc433
zibrah3ed <zibrah3ed@gmail.com>
2016-02-24 15:57:50 -0600

    Added files via upload

--- ex36homework_game.rb
+++ ex36homework_game.rb
@@ -2,13 +2,116 @@
 
 # Import file at run called seed.txt.
 
-# Check to see if nouns_list was populated correctly.---> nouns_list.each {|i| puts i}
-# key = []
-# (0..19).each {|i| key << i}
-# key_scramble = key.shuffle
-# rand_index = rand(19)
-# print key_scramble
-# another test key_scramble.each {|i| print rand_index, "\t", nouns_list[rand_index], "\n"}
+# Monster Array
+
+
+def meet_monster
+  mons_a = ["Ogre", "Orc", "Gnome", "Bob", "Demon", "Hrrrd", "Ikor"]
+  m_hp = 100
+  hp = 100
+  monster = mons_a[rand(mons_a.length)] # Pick random monster based on array length.
+
+  puts "!!_____#{monster}_____!!\n\n\n\n\n\a"
+  puts "Your HP : #{hp}\n#{monster} HP : #{m_hp}\n"
+
+      while m_hp > 0 && hp > 0
+
+        print "(a)ttack, (d)odge, or (R)un away: "
+        print "> "
+        choice = $stdin.gets.chomp
+
+        if choice.downcase == 'a'
+          m_hp = attack_monster(m_hp,100,monster)
+          hp = attack_ppl_mon(hp,100,monster)
+          puts "The #{monster}'s hp is now : \t\t#{m_hp}\n"
+          puts "The Your hp is now : \t\t\t#{hp}\n\n"
+
+        elsif choice.downcase == 'd'
+          m_hp = defend_monster(m_hp,100,monster)
+          hp = defend_ppl_mon(hp,100,monster)
+          puts "The #{monster}'s hp is now : \t\t#{m_hp}\n"
+          puts "The Your hp is now : \t\t\t#{hp}\n\n"
+
+        elsif choice.downcase == 'r'
+            dead("Running from fights again heh, death awaits at the end of your jog.") # Change later simply a place holder, meet_boss
+
+        else
+            dead("Reading is hard you die.")
+        end
+      end
+
+      if hp <= 0
+        dead("Your hp has reached 0.")
+      else
+        puts "#{monster} defeated. Yay! You!\n"
+        rand_hall
+
+      end
+end
+
+def attack_monster(hit_points,chance,monster)
+  if hit_points > 0
+    if rand(chance) >= chance * 2 / 3
+      puts "Successfull attack for 25 dmg.\n"
+      hit_points -= 25
+      return hit_points
+    else
+      puts "You missed the #{monster}.\n"
+      return hit_points
+    end
+  else
+    return dead()
+    exit (1)
+  end
+end
+
+def attack_ppl_mon(hit_points,chance,monster)
+  if hit_points > 0
+    if rand(chance) >= chance * 3 / 4
+      puts "#{monster}'s attack suceeds. You take 10 dmg.\n"
+      hit_points -= 10
+      return hit_points
+    else
+      puts "The #{monster} has poor aim.\n"
+      return hit_points
+    end
+  else
+    return dead()
+    exit (8)
+  end
+end
+
+def defend_monster(hit_points,chance,monster)
+  if hit_points > 0
+    if rand(chance) <= chance * 3 / 4
+      puts "Successfull dodge.\n"
+      return hit_points
+    else
+      hit_points -= 5
+      puts "You parry the #{monster}'s thrust... doing 5 dmg.\n"
+      return hit_points
+    end
+  else
+    return dead()
+    exit (2)
+  end
+end
+
+def defend_ppl_mon(hit_points,chance,monster)
+  if hit_points > 0
+    if rand(chance) <= chance * 3 / 4
+      puts "Successfull dodge.\n"
+      return hit_points
+    else
+      hit_points -= 5
+      puts "The #{monster}'s attack grazes your cheek 5 dmg.\n"
+      return hit_points
+    end
+  else
+    return dead()
+    exit (10)
+  end
+end
 
 def start #Main method that starts game, and loads random text.
 
@@ -43,53 +146,65 @@ end
 def rand_hall #Attempt to add depth to game
     hall_rand = rand(5) + 1
      if hall_rand == 1
-        hall_1
+        hall_1(100)
      elsif hall_rand == 2
-        hall_2
+        hall_2(100)
      elsif hall_rand == 3
-        hall_3
+        hall_3(100)
      elsif hall_rand >= 4
-        hall_4
+        hall_4(100)
      else
          dead ("How did this kill you.....")
      end
 end
 
-def hall_1 # Rand_hall with only choice to go forward
+def hall_1(chance) # Rand_hall with only choice to go forward
     puts "The hall before you is clear."
     puts "There are no exits or doors visible go (f)orward or die. "
 
     choice = $stdin.gets.chomp
+    chance1 = chance
+    chance = rand(chance)
 
         if choice.downcase == 'f'
-            rand_hall
+            if chance <= chance1 / 2
+              meet_monster()
+            else
+              rand_hall
+          end
         else
             dead ("Life apparently isn't for you. H1")
         end
 end
 
-def hall_2 # Rand_hall with door on the left
+def hall_2(chance) # Rand_hall with door on the left
     puts "The hall before you is clear."
     puts "There is a door on your left."
     puts "Go (l)eft or (f)orward"
 
+    chance1 = chance
     choice = $stdin.gets.chomp
-    rando2 = rand(100)
+    chance = rand(chance)
+    chance2 = rand(chance)
 
         if choice.downcase == 'f'
-            if rando2 <= 80
+            if chance <= chance * 1 / 3
               rand_hall
-            elsif rando2 > 80
+            elsif chance > chance1 * 1 / 3 && chance <= chance1 * 2 / 3
               room_1
             else
               meet_a_knight
             end
 
         elsif choice.downcase == 'l'
-            if rando2 <= 85
+            if chance <= chance1 * 4 / 5
               rand_hall
-            elsif rando2 > 85
+            elsif chance > chance * 4 / 5
+              if chance > chance2
               meet_baby_dragon
+              else
+              meet_monster()
+              end
             else
               room_1
             end
@@ -98,28 +213,35 @@ def hall_2 # Rand_hall with door on the left
         end
 end
 
-def hall_3 # Rand_hall with door on the right
+def hall_3(chance) # Rand_hall with door on the right
     puts "The hall before you is clear."
     puts "There is a door on your right."
     puts "Go (r)ight or (f)orward"
 
+
     choice = $stdin.gets.chomp
-    rando2 = rand(100)
+    chance1 = chance
+    chance = rand(chance)
+    chance2 = rand(chance)
 
         if choice.downcase == 'f'
-            if rando2 <= 80
+            if chance <= chance1 * 4 / 5
               rand_hall
-            elsif rando2 > 80
-              room_1
+            elsif chance > chance1 * 4 / 5
+              meet_monster
             else
               meet_a_knight
             end
 
         elsif choice.downcase == 'r'
-            if rando2 <= 75
+            if chance <= chance1 * 3 / 4
               rand_hall
-            elsif rando2 >75
-              meet_baby_dragon
+            elsif chance > chance1 * 3 / 4
+              if chance > chance2
+                meet_baby_dragon
+              else
+                meet_monster
+              end
             else
               room_1
             end
@@ -128,37 +250,51 @@ def hall_3 # Rand_hall with door on the right
         end
 end
 
-def hall_4 # Rand_hall with door on the left and right
+def hall_4(chance) # Rand_hall with door on the left and right
     puts "The hall before you is clear."
     puts "There is a door on your left and right."
     puts "Go (l)eft, (r)ight or (f)orward"
-
+    chance1 = chance
     choice = $stdin.gets.chomp
-    rando2 = rand(100)
+    chance = rand(chance)
+    chance2 = rand(chance)
+
 
         if choice.downcase == 'f'
-            if rando2 <= 50
+            if chance <= chance1 / 2
               rand_hall
-            elsif rando2 > 50
-              room_1
+            elsif chance > chance1 /2
+              if chance2 > chance
+                meet_monster
+              else
+                room_3
+              end
             else
               meet_a_knight
             end
 
         elsif choice.downcase == 'l'
-            if rando2 <= 50
+            if chance <= chance1 / 2
               rand_hall
-            elsif rando2 > 50
-              meet_baby_dragon
+            elsif chance > chance1 / 2
+              if chance2 > chance
+                meet_monster
+              else
+                meet_baby_dragon
+              end
             else
               room_1
             end
 
-          elsif choice.downcase == 'l'
-              if rando2 <= 50
+          elsif choice.downcase == 'r'
+              if chance <= chance1 / 2
                 rand_hall
-              elsif rando2 > 50
-                meet_baby_dragon
+              elsif chance > chance1 / 2
+                if chance2 > chance
+                  meet_monster
+                else
+                  rand_hall
+                end
               else
                 room_3
               end
@@ -251,14 +387,22 @@ end
 def meet_baby_dragon
 
   puts "You meet a baby DRAGON!! Smoke curls from it's malformed nostrils."
-  print "(f)ight, (d)o nohting, or (R)un away: "
+  print "(f)ight, (d)o nothing, or (R)un away: "
   print "> "
   choice = $stdin.gets.chomp
 
   if choice.downcase == 'f'
-      dead("Turns out your armor makes is not defense against flame. Broiled knight.")
+      puts "Baby dragon runs away another monster comes \nto it's aid."
+      meet_monster
   elsif choice.downcase == 'd'
-      dead("Doing nothing in the face of danger! You die for cowardice and the large\nbits the dragon bit from your side.")
+      chance = rand(100)
+      if chance >= 90
+        puts "Along comes Momma."
+        meet_boss
+      else
+        puts "Your indecision is rewarded with a monster FIGHT!."
+        meet_monster
+      end
   elsif choice.downcase == 'r'
       puts "You run away, this actually is the best choice.....\nUntil you hear a strange rumbling in front of you.\n\n\n"
       meet_boss # Change later simply a place holder, meet_boss
@@ -371,8 +515,6 @@ def meet_boss # Not yet down may need to break into further parts for ease of un
         exit(4)
         end
 end
-
-
 
 # Print window dressing and title for game, Cart before horse since this was second thing coded for this program.
 # Window Dressing
